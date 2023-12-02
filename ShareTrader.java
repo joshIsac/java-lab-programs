96baa3489e910e76e241872a3dff24c7de85ab14
joshIsac <141471475+joshIsac@users.noreply.github.com>
2023-12-02 07:48:49 +0530

    ShareTrader.java

--- ShareTrader.java
+++ ShareTrader.java
@@ -1,67 +1,45 @@
 import java.util.Scanner;
 
 public class ShareTrader {
-    private static int maxProfit;
-
-    public static int findMaxProfit(int[] prices) {
-        maxProfit = 0;
-        calculateMaxProfit(prices, 0, 0, 0);
-        return maxProfit;
-    }
-
-    private static void calculateMaxProfit(int[] prices, int day, int transactions, int currentProfit) {
-        if (day == prices.length || transactions == 2) {
-            if (currentProfit > maxProfit) {
-                maxProfit = currentProfit;
-            }
-            return;
-        }
-
-        calculateMaxProfit(prices, day + 1, transactions, currentProfit);
-
-        for (int sellDay = day + 1; sellDay < prices.length; sellDay++) {
-            if (prices[sellDay] > prices[day]) {
-                int profit = prices[sellDay] - prices[day];
-                calculateMaxProfit(prices, sellDay + 1, transactions + 1, currentProfit + profit);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the number of stock prices: ");
         int n = scanner.nextInt();
 
-        int[] stockPrices = new int[n];
+        int[] prices = new int[n];
         System.out.println("Enter the stock prices:");
         for (int i = 0; i < n; i++) {
-            stockPrices[i] = scanner.nextInt();
+            prices[i] = scanner.nextInt();
         }
 
-        int result = findMaxProfit(stockPrices);
-        System.out.println("Maximum Profit: " + result);
-
-        // Display transactions
-        System.out.println("Trader earns " + maxProfit + " as sum of:");
-        findAndDisplayTransactions(stockPrices, result);
+        calculateAndPrintMaxProfit(prices);
 
         scanner.close();
     }
 
-    private static void findAndDisplayTransactions(int[] prices, int totalProfit) {
-        int currentProfit = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (currentProfit == totalProfit) {
-                break;
-            }
-            for (int j = i + 1; j < prices.length; j++) {
-                if (prices[j] > prices[i] && (prices[j] - prices[i] + currentProfit) == totalProfit) {
-                    System.out.println("Buy at " + prices[i] + ", sell at " + prices[j]);
-                    currentProfit += prices[j] - prices[i];
-                    break;
-                }
-            }
+    private static void calculateAndPrintMaxProfit(int[] prices) {
+        int n = prices.length;
+        if (n < 2) {
+            System.out.println("Not enough prices to trade.");
+            return;
         }
+
+        int buy1 = Integer.MAX_VALUE, sell1 = 0;
+        int buy2 = Integer.MAX_VALUE, sell2 = 0;
+
+        for (int price : prices) {
+            buy1 = Math.min(buy1, price);
+            sell1 = Math.max(sell1, price - buy1);
+            buy2 = Math.min(buy2, price - sell1);
+            sell2 = Math.max(sell2, price - buy2);
+        }
+
+        int totalProfit = sell1 + sell2;
+
+        System.out.println("Output: " + totalProfit);
+        System.out.println("Trader earns " + totalProfit + " as sum of " + sell1 + " and " + sell2);
+        System.out.println("Buy at " + prices[0] + ", sell at " + (prices[0] + sell1) + ",");
+        System.out.println("Buy at " + (prices[0] + sell1 - buy2) + " and sell at " + (prices[0] + sell1 + sell2));
     }
 }
